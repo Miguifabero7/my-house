89c25e3d8d2d636b74b0f80c13506f05e7aec374
Miguifabero7 <miguiperlop7@gmail.com>
2018-10-17 13:05:22 +0200

    Ahora hay un campo verde debajo de la casa, el sol aparece lo mas cercano a este campo y el sol imita al amanecer

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Square grass;
 
     /**
      * Constructor for objects of class Picture
@@ -48,12 +49,20 @@ public class Picture
         roof.moveVertical(-60);
         roof.makeVisible();
 
+        grass = new Square();
+        grass.changeColor("green");
+        grass.moveHorizontal(-340);
+        grass.moveVertical(140);
+        grass.changeSize(580);
+        grass.makeVisible();
+        
         sun = new Circle();
         sun.changeColor("yellow");
         sun.moveHorizontal(100);
-        sun.moveVertical(-40);
+        sun.moveVertical(90);
         sun.changeSize(80);
         sun.makeVisible();
+        sun.slowMoveVertical(-170);
     }
 
     /**
